f38f9c556f1dcf9aed7c273f45eae24dcc43717a
Chris Adams <chris.adams@nasa.gov>
2009-11-17 22:21:40 +0800

    Bugfix: social_bookmarking_tags now uses HttpRequest.build_absolute_url()
    Before it relied on some assumptions about what's in request.META which
    aren't true in the Django test client

--- social_bookmarking/templatetags/social_bookmarking_tags.py
+++ social_bookmarking/templatetags/social_bookmarking_tags.py
@@ -1,4 +1,4 @@
-from sys import stderr
+import urlparse
 
 from django.template import Library
 from django.utils.http import urlquote
@@ -15,51 +15,25 @@ def show_bookmarks(context, title, object_or_url, description=""):
     """ Displays the bookmarks
         TODO: Add in the javascript cleanup part
     """
-    
-    if isinstance(object_or_url, (str, unicode)):
-        url = unicode(object_or_url)
-    else:
-        url = unicode(getattr(object_or_url, 'get_absolute_url')())        
-        
-    if not url.startswith('http'):
-        # URL does not have leading http so we need to find the http host
-            
-        http_host = None
-        for element in context.dicts:
-            if 'request' in element:
-                try:
-                    http_host = element['request'].META['HTTP_HOST']    
-                except AttributeError:
-                    msg = """
-                            social_bookmarking_tag could not find the META attribute
-                            on the http request object."""
-                    raise AttributeError(msg)
-                except KeyError:
-                    msg = """
-                            social_bookmarking_tag could not find the HTTP_HOST 
-                            key on the META attribute of the http request object."""
-                    raise KeyError(msg)
-                
-                break
-    
-        if not http_host:
-            msg = """
-                    No request given to the social_bookmarking_tags template tag. 
-                    Please add 'django.core.context_processors.request' to the
-                    TEMPLATE_CONTEXT_PROCESSORS in your settings.py file."""
-            raise NoRequestContextProcessorFound(msg)
-            
-        url = 'http://' + http_host + url
 
-    bookmarks = Bookmark.objects.filter(status=2).values()
+    if hasattr(object_or_url, 'get_absolute_url'):
+        url = getattr(object_or_url, 'get_absolute_url')()
+
+    url = unicode(object_or_url)
     
+    if not url.startswith('http'):
+        url = context['request'].build_absolute_uri(url)
+
+    # TODO: Bookmark should have a .active manager:
+    bookmarks = Bookmark.objects.filter(status=2).values()
+
     for bookmark in bookmarks:
-        bookmark['description'] = description     
+        bookmark['description'] = description
         bookmark['link'] = bookmark['url'] % {'title': urlquote(title),
                                         'url': urlquote(url),
                                         'description': urlquote(description)
                                        }
-        
-        
+
+
     return {'bookmarks':bookmarks, 'MEDIA_URL': context['MEDIA_URL']}
-        
+
