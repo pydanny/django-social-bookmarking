7d89fd84d607ae4e13ee969345bee4b84b3b1265
Daniel Greenfeld <pydanny@gmail.com>
2009-11-19 13:37:08 -0500

    Adding in test to confirm that the social_bookmarking_tags.show_bookmarks function is working properly

---
+++ social_bookmarking/tests.py
@@ -0,0 +1,46 @@
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.template import Context, Template
+from django.template.loader import get_template_from_string
+from django.test import TestCase
+
+
+class TestBookmarks(TestCase):
+    fixtures = ['bookmarks.json',]
+    
+    def setUp(self):
+        
+        self.user = User(username="bookmark_user")
+        self.user.save()
+        
+        self.google_test = """<span class="bookmark" id="google">
+                
+                <a href="http://www.google.com/bookmarks/mark?op=edit&amp;bkmk=http%3A//python.org&amp;title=bookmark_user" title="Google" rel="nofollow">
+                    <img src="/media/social_bookmarking/img/googlebookmark.png" width="16" height="16" alt="Google" border="0" />
+                </a>
+        </span>"""
+        
+        self.print_test = """<span class="bookmark" id="print">
+                
+                <a href="javascript:window.print();" title="Print" rel="nofollow">
+                    <img src="/media/social_bookmarking/img/printer.png" width="16" height="16" alt="Print" border="0" />
+                </a>
+        </span>"""
+
+    
+    def tearDown(self):
+        pass
+    
+    def test_template_tag(self):
+        """ Does the template tag work without blowing up? """
+        
+        template = get_template_from_string("""
+            {% load social_bookmarking_tags %}
+            
+            {% show_bookmarks object.username 'http://python.org' %}
+        """)
+        
+        c = Context({'object':self.user, 'MEDIA_URL':settings.MEDIA_URL})
+        html = template.render(c)
+        self.assertTrue(self.google_test in html)
+        self.assertTrue(self.print_test in html)        
